63a76a6f535cc03553b575577acb74779b7adc4c
saehim <saeilee.vxv@gmail.com>
2022-10-02 23:28:04 +0900

    :memo: 11회분 풀이

---
+++ sw/1204.py
@@ -0,0 +1,6 @@
+# 1204.py
+# 최빈수 구하기 https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV13zo1KAAACFAYh&categoryId=AV13zo1KAAACFAYh&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=1&&&&&&&&&&
+
+t = int(input())
+
+for _ in range():
