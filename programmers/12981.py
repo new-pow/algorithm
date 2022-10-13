9ee43291ebe3bea9cc993264d447e87111930438
saehim <saeilee.vxv@gmail.com>
2022-10-13 23:29:48 +0900

    :memo: 14회차 풀이분 : 영어 끝말잇기

---
+++ programmers/12981.py
@@ -0,0 +1,14 @@
+# 12981.py
+# 영어 끝말잇기 https://school.programmers.co.kr/learn/courses/30/lessons/12981
+
+def solution(n, words):
+    answer = [0, 0]
+    turns = [0]*n
+    print(turns)
+
+    return answer
+
+n = 3
+words = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
+
+solution(n, words)
