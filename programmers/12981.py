289219bd2e747f70c3bce9f099acb22516fbca70
saehim <saeilee.vxv@gmail.com>
2022-10-15 02:01:12 +0900

    :memo: 15회차 풀이분 : 영어 끝말잇기

--- programmers/12981.py
+++ programmers/12981.py
@@ -4,11 +4,34 @@
 def solution(n, words):
     answer = [0, 0]
     turns = [0]*n
-    print(turns)
+    says = []
+
+    says.append(words[0])
+
+    for i in range(1, len(words)):
+        turn = (i%n)+1
+        turns[i%n] += 1 # 차례 카운트
+        if words[i] in says or words[i-1][-1]!=words[i][0]:
+            return [turn, turns[i%n]]
+        
+        says.append(words[i])
 
     return answer
 
 n = 3
 words = ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]
 
-solution(n, words)
+print(solution(n, words))
+
+# def solution(n, words) :
+#     turn = 1 # 처음 순서
+#     before = words[0][-1]
+#     for i in range(1, len(words)) :
+#         # 단어의 시작이 잘못 되었거나 이전에 이미 말한적이 있다면
+#         if words[i][0] != before or words[i] in words[:i] :
+#             a, b = divmod(i, n) # 몫, 나머지
+#             return [b + 1, a + 1]
+#         else :
+#             before = words[i][-1]
+
+#     return [0, 0]
