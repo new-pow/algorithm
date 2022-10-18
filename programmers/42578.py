7c3cdeab9f7af1d281706365373c601286bdd1e6
saehim <saeilee.vxv@gmail.com>
2022-10-18 13:58:39 +0900

    :memo: 16회차 풀이분 : 위장

---
+++ programmers/42578.py
@@ -0,0 +1,9 @@
+# 42578.py
+# 위장 https://school.programmers.co.kr/learn/courses/30/lessons/42578
+# 1회차 풀이분 : 
+
+def solution(clothes):
+    answer = 0
+    return answer
+
+clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	
