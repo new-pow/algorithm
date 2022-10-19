2fed3ff28c98614d64d8a00810aa08d21597cae5
saehim <saeilee.vxv@gmail.com>
2022-10-19 23:29:56 +0900

    :memo: 16회차 풀이 : 위장

--- programmers/42578.py
+++ programmers/42578.py
@@ -3,7 +3,21 @@
 # 1회차 풀이분 : 
 
 def solution(clothes):
-    answer = 0
+    sets = {}
+    for i in range(len(clothes)):
+        if clothes[i][1] not in sets:
+            sets[clothes[i][1]] = 2
+        else:
+            sets[clothes[i][1]] += 1
+    
+    answer = 1
+
+    for i in sets.values():
+        answer *= i # 경우의 수
+    answer -= 1 # 모든 요소를 착용하지 않을 때
+
     return answer
 
 clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	
+
+print(solution(clothes))
