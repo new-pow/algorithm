9fc6a53fd97fcd60a79b23559565d1cd4a322ef8
new-pow <103120173+new-pow@users.noreply.github.com>
2022-10-31 17:59:03 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 152 ms, Memory: 15296 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -5,12 +5,12 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        for (int i=1; i<n+1; i++) {
+        for (int i=0; i<n; i++) {
             String stars ="";
-            for (int j=1; j<i; j++) {
+            for (int j=0; j<i; j++) {
                 stars += " ";
             }
-            for (int j=n; j>=i; j--) {
+            for (int j=n; j>i; j--) {
                 stars += "*";
             }
             System.out.println(stars);
