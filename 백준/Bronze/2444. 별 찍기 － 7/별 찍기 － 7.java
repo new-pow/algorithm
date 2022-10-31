20863e2b777ed908c92def28652b5cb64519cb01
new-pow <103120173+new-pow@users.noreply.github.com>
2022-11-01 01:50:22 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i; j++){
+                bw.write(" ");
+            }
+            for(int k = 1; k<2*i; k++){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        for(int i = n-1; i>=1; i--){
+            for(int j = 1; j<=n-i; j++){
+                bw.write(" ");
+            }
+            for(int k = 2*i-1; k>0; k--){
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
