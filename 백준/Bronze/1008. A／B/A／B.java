feb104c52b7fd7f43c90ca1cb00d6c2a52a8db58
new-pow <103120173+new-pow@users.noreply.github.com>
2022-11-02 16:27:21 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+        
+		if (0<a && b<10) {
+            bw.write(String.valueOf(a/b));          
+        }
+        bw.flush();
+        bw.close();
+	}
+}
