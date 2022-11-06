8b296e5b6d485d79e361519eedf45eb7947ebc01
new-pow <103120173+new-pow@users.noreply.github.com>
2022-11-06 14:05:18 +0900

    [Silver I] Title: 절댓값 힙, Time: 576 ms, Memory: 26212 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            if(Math.abs(o1) > Math.abs(o2)) {
+                return Math.abs(o1) - Math.abs(o2);
+                //절대값 기준으로 두 값이 같다면 음수를 앞으로 보내준다.
+            }else if(Math.abs(o1) == Math.abs(o2)) {
+                return o1 - o2;
+            }else {
+                return -1;
+            }
+        });
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine()); // 연산 개수
+
+        for(int i=0; i<n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x!=0) {
+                pq.add(x);
+            } else {
+                if (pq.isEmpty()) System.out.println(0);
+                else System.out.println(pq.poll());
+            }
+        }
+    }
+
+}
