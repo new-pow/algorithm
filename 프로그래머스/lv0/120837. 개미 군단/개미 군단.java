0807166e7347669a9166adce6164dd90ca05e99d
new-pow <103120173+new-pow@users.noreply.github.com>
2022-11-27 13:38:43 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        int[] ants = {5,3,1};
+        
+        for (int i=0; i<3; i++) {
+            if (hp>=ants[i]) {
+            answer += hp/ants[i];
+            hp %= ants[i];
+            }
+        }
+        
+        return answer;
+    }
+}
